318c4d2359fcd6e2a93c697df0b536feed41f102
toru1055 <toru1055h@gmail.com>
2016-03-06 09:41:34 +0900

    debug

--- src/main/java/jp/thotta/oml/server/ml/LearnerFactory.java
+++ src/main/java/jp/thotta/oml/server/ml/LearnerFactory.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Properties;
 import java.util.HashMap;
-import jp.thotta.oml.server.input.LabelFactory;
+import jp.thotta.oml.server.io.LabelFactory;
 import jp.thotta.oml.server.admin.PathManager;
 
 /**
